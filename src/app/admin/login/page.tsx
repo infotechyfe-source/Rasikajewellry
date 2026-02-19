c2dced0c8627f59dc88ecd1c7dcbb052a57cbce1
Parveendhillon <parveendhillo08@gmail.com>
2026-02-19 09:58:19 +0530

    commit

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -23,6 +23,7 @@ export default function AdminLogin() {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),
+        credentials: 'include', 
       });
 
       const data = await res.json();
